81f2ceda252f2af31cde3aaffa23eaf33edd4b9b
ErnestBidouille <theo.chennebault@gmail.com>
2021-02-09 17:57:17 +0100

    Trying to solve build error

--- install_spark.sh
+++ install_spark.sh
@@ -4,4 +4,4 @@ SPARK_VERSION='3.0.1'
 HADOOP_VERSION='2.7'
 
 curl http://apache.mirror.gtcomm.net/spark/spark-$SPARK_VERSION/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz --output /tmp/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz
-cd /tmp && tar -xvzf /tmp/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz
+cd /tmp && tar xzf /tmp/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz
