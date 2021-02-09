f56dd4a4fd735288ccc3ab2ede90ecfcbfa38b56
ErnestBidouille <theo.chennebault@gmail.com>
2021-02-09 19:39:47 +0100

    Added some codecov

--- tests/test_ccf_spark.py
+++ tests/test_ccf_spark.py
@@ -45,9 +45,9 @@ def test_ccf_ccf_spark_iterate():
     graph = CcfSpark(
         sc,
         file_path=os.path.join(data_path, 'test_graph.txt'),
-        secondary_sorting=True
+        secondary_sorting=True,
     )
-    graph.iterate()
+    graph.iterate(with_distinct=True)
     expected = [(2, 1), (3, 1), (4, 1), (3, 2), (4, 2), (5, 2), (5, 4), (7, 6),
                 (8, 6), (8, 7)]
     assert len(graph.print()) == len(expected)
@@ -66,9 +66,9 @@ def test_ccf_ccf_spark_iterate_all():
     graph = CcfSpark(
         sc,
         file_path=os.path.join(data_path, 'test_graph.txt'),
-        secondary_sorting=True
+        secondary_sorting=True,
     )
-    graph.iterate_all()
+    graph.iterate_all(with_distinct=True)
     expected = [(2, 1), (3, 1), (4, 1), (5, 1), (7, 6), (8, 6)]
     assert len(graph.print()) == len(expected)
     assert set(graph.print()) == set(expected)
@@ -88,9 +88,10 @@ def test_ccf_ccf_spark_number_of_connected_components():
         file_path=os.path.join(data_path, 'test_graph.txt'),
     )
     assert graph.number_of_connected_components() == 2
-    graph_generator = GraphGenerator.generate_ccf_random_graph(15,20)
+    graph_generator = GraphGenerator.generate_ccf_random_graph(15, 20)
     graph = CcfSpark(
-        sc, 
-        graph = graph_generator,
+        sc,
+        graph=graph_generator,
     )
-    assert graph.number_of_connected_components() == nx.number_connected_components(graph_generator.graph)
+    assert graph.number_of_connected_components(
+    ) == nx.number_connected_components(graph_generator.graph)
